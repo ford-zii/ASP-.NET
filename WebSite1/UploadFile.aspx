3c7e4303e17a3ffd688fe31e116d4d7c5281346c
ford-zii <shissanupong@outlook.com>
2019-03-30 14:20:36 +0700

    uploadFile simple

---
+++ WebSite1/UploadFile.aspx
@@ -0,0 +1,12 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="UploadFile.aspx.cs" Inherits="UploadFile" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <asp:FileUpload ID="FileUpload1" runat="server" Height="40px" />
+    <asp:Button ID="btnUpload" runat="server" Height="36px" OnClick="btnUpload_Click" Text="Upload" />
+&nbsp;
+    <asp:Label ID="lableSuc" runat="server" ForeColor="Lime" Visible="False"></asp:Label>
+&nbsp;
+</asp:Content>
+
---
+++ WebSite1/UploadFile.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+
+public partial class UploadFile : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnUpload_Click(object sender, EventArgs e)
+    {
+        lableSuc.Visible = true;
+        if (FileUpload1.HasFile)
+        {
+            string Ext = System.IO.Path.GetExtension(FileUpload1.PostedFile.FileName); //get ext
+            if(Ext.ToUpper() == ".JPG")
+            {
+                string Newname = Guid.NewGuid().ToString(); //gen name
+                string cNewname = string.Format("{0}{1}", Newname, Ext); // create name+ext
+                string Path = "Upload/"; //Path Upload
+                string cPath = Server.MapPath(string.Format("{0}{1}", Path, cNewname));
+                FileUpload1.SaveAs(cPath);
+                lableSuc.ForeColor = System.Drawing.Color.Lime;
+                lableSuc.Text = "Success!!!";
+            }
+            else
+            {
+                lableSuc.ForeColor = System.Drawing.Color.Red;
+                lableSuc.Text = "File not support!!.";
+            }
+            
+        }
+        else
+        {
+         
+            lableSuc.ForeColor = System.Drawing.Color.Red;
+            lableSuc.Text = "Miss file";
+        }
+        
+    }
+}
